f7e057a5867cd8b1e8a4f88ad3b10680faa83891
Likem <likemdzokoto@gmail.com>
2022-07-28 19:22:40 +0000

    chore: add interface contract to define functions

--- contracts/likemToken.sol
+++ contracts/likemToken.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+/**
+*@title Token - smart contract for likemToken(erc20 token standard)
+*/
 contract likemToken {
+  string public name = "Likem Token";
+  string public symbol = "$LTK";
+  uint256 public totalSupply;
+  
+  
+  
+  
   constructor() public {
   }
 }

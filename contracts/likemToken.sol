cd9262f1a3e3f0d194409a6cc48a66ffb5608ff9
Likem <likemdzokoto@gmail.com>
2022-08-10 18:07:06 +0000

    done with transfer function

--- contracts/likemToken.sol
+++ contracts/likemToken.sol
@@ -1,13 +1,47 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity  >=0.4.22 <0.9.0;
+
 
 /**
 *@title Token - smart contract for likemToken(erc20 token standard)
 */
-contract likemToken {
+
+
+//import interface and ownable contract to be inherited into token contract
+import "./likemTokenInterface.sol";
+import "./ownable.sol";
+
+contract likemToken  is likemTokenInterface,Ownable{
   string public name = "Likem Token";
   string public symbol = "$LTK";
   uint256 public totalSupply;
+
+  mapping (address => uint256) public balanceOf;
+  mapping (address => uint256) public allowance;
+
+
+
+  //constructor 
+  constructor(uint256 _initialSupply) public{
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply  = _initialSupply; 
+  }
+
+  //transfer function 
+  function transfer(address _to , uint256 _value) public virtual override  returns  (bool success){
+    require(balanceOf[msg.sender] >= _value,"insufficient funds");
+    balanceOf[msg.sender] -= _value;
+    balanceOf(_to) +=_value;
+
+  }
+
+
+
+
+
+
+
+
   
   
   

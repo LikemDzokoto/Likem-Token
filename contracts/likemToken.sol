1eaa76e065d32be927b895aebc8d0e1134b5a9bd
Likem <likemdzokoto@gmail.com>
2022-08-10 17:08:09 +0000

    feat(add interface contract to define functions): :art: add interface contract to define functions~

--- contracts/likemToken.sol
+++ contracts/likemToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.4;
 
 /**
 *@title Token - smart contract for likemToken(erc20 token standard)
@@ -8,6 +8,7 @@ contract likemToken {
   string public name = "Likem Token";
   string public symbol = "$LTK";
   uint256 public totalSupply;
+  
   
   
   
